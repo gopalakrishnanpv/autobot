e926217a7bb8ee509d9f00b530659aec21bd3cb9
Panchavarnam Gopalakrishnan <gopalakrishnan.panchavarnam@mt.com>
2023-06-11 16:12:00 +0530

    Add email validation

--- src/base/input.jsx
+++ src/base/input.jsx
@@ -3,7 +3,7 @@ export default function Input(props) {
         <div>
             <p className="font-medium my-3">{props.title}</p>
             <input type={props.type} className="p-2 w-full rounded border border-gray-300 shadow-inner shadow-gray-100 bg-gray-50" id={props.id}
-                placeholder={props.placeholder} />
+                onKeyUp={props.onKeyUp} placeholder={props.placeholder} />
         </div>
     );
 }
--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -5,16 +5,33 @@ import Input from "../base/input";
 import RadioButtonGroup from "../base/radiobutton-group";
 import Range from "../base/range";
 
+function validateEmail() {
+    var email = document.getElementById("email").value;
+    var lblError = document.getElementById("emailError");
+    lblError.innerHTML = "";
+    var expr = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if (email.length == 0) {
+        lblError.innerHTML = "";
+        return;
+    }
+    if (!expr.test(email)) {
+        lblError.innerHTML = "Invalid email address.";
+    }
+}
 
 export default function Inputs(props) {
     return (
         <div className="p-5">
             <Heading text="Inputs" />
             <div
-                className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 flex gap-5 align-center justify-start">
+                className="grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-5 align-center justify-start">
                 <Input type="text" id="name" title="Text" placeholder="Enter your name" />
                 <Input type="number" id="age" title="Number" placeholder="Enter your age" />
-                <Input type="email" id="email" title="Email" placeholder="Enter your email" />
+                <div>
+                    <Input type="email" id="email" title="Email" placeholder="Enter your email"
+                        onKeyUp={validateEmail} />
+                    <p id="emailError" className="text-sm mt-2 text-red-600"></p>
+                </div>
                 <Input type="password" id="password" title="Password" placeholder="Enter your password" />
                 <Dropdown />
                 <Input type="date" id="date" title="Date" />
