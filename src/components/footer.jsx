7d981015f1db7cf227a569620c92a89e8943299b
Panchavarnam Gopalakrishnan <gopalakrishnan.panchavarnam@mt.com>
2023-06-11 13:30:21 +0530

    Update footer message

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer(props) {
     return <footer className="text-center p-2 bg-violet-700 text-white">
-        <p>© 2023 Automation Practice. All rights reserved.</p>
+        <p>© 2023 Autobot. All rights reserved.</p>
     </footer>
 }
