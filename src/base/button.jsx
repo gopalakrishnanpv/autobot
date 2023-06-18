dbec9b84d5e0eee64036cf42fa08426de4a3de6d
Panchavarnam Gopalakrishnan <gopalakrishnan.panchavarnam@mt.com>
2023-06-18 19:01:49 +0530

    update context click function

--- src/base/button.jsx
+++ src/base/button.jsx
@@ -2,7 +2,7 @@ export default function Button(props) {
     return (
         <div className="flex flex-col align-start justify-start">
             <p className="font-medium my-3 ">{props.title}</p>
-            <button id={props.id} onClick={props.onClick} onContextMenu={props.onContext}
+            <button id={props.id} onClick={props.onClick} onContextMenu={props.onContextMenu}
                 className={`uppercase text-center text-sm bg-indigo-600 rounded hover:bg-indigo-700 shadow-sm shadow-indigo-600 text-white font-medium py-4 ${props.className}`}>
                 <a href={props.href} target={props.target} rel="noopener noreferrer"
                     className="text-sm block">
